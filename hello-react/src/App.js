83c0dd13f7ef4654904288851a6d00da56bed48f
dean-kim <keunu07@gmail.com>
2018-07-28 07:52:24 +0900

    end basic handling of state

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import MyName from './MyName';
-
+// import MyName from './MyName';
+import Counter from './Counter'
 
 class App extends Component {
   render() {
       return (
-          <MyName name='React' />
+          <Counter />
       );
   }
 }
