871b59567e3864191d7fb89323dbd8423da8bbfa
dean-kim <keunu07@gmail.com>
2018-08-02 08:55:04 +0900

    using styled-component

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Button from "./components/Button";
+import StyledButton from "./components/StyledButton";
 
 
 class App extends Component {
   render() {
       return (
           <div>
-              <Button>BUTTON</Button>
+              <StyledButton big>BUTTON</StyledButton>
           </div>
       )
   }
