7d25f7013c21e8dc73188b4bf155feaec69cc3f2
dean-kim <keunu07@gmail.com>
2018-08-02 08:47:02 +0900

    using Sass lib

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
-import classNames from 'classnames/bind';
-import styles from './App.scss';
-const cx = classNames.bind(styles);
-
-console.log(styles);
-console.log(cx('box', 'blue'));
+import Button from "./components/Button";
 
 
 class App extends Component {
   render() {
-      const isBlue = true;
-
-    return (
-        <div className={cx('box', {
-            blue: isBlue
-        })}>
-            <div className={cx('box-inside')}/>
-
-        </div>
-    );
+      return (
+          <div>
+              <Button>BUTTON</Button>
+          </div>
+      )
   }
 }
 
