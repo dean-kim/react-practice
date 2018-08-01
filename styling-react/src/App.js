27d36e9594ff3f89454060e1ece3955324572c54
dean-kim <keunu07@gmail.com>
2018-08-01 16:59:01 +0900

    add classnames.bind with css

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import classNames from 'classnames/bind';
 import styles from './App.css';
+const cx = classNames.bind(styles);
 
 console.log(styles);
+console.log(cx('box', 'blue'));
+
 
 class App extends Component {
   render() {
     return (
-        <div className={[styles.box, styles.blue].join(' ')}>
+        <div className={cx('box', 'blue')}>
 
         </div>
     );
