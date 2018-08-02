fa351214e60db73f4c5689f727f38ec0875b22bd
dean-kim <keunu07@gmail.com>
2018-08-03 08:15:31 +0900

    make ViewerTemplate

--- nasa-apod/src/components/ViewerTemplate/ViewerTemplate.js
+++ nasa-apod/src/components/ViewerTemplate/ViewerTemplate.js
@@ -4,12 +4,19 @@ import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-const ViewerTemplate = ({ viewer, spaceNavigator }) => {
-    return (
-        <div className={cx('viewer-template')}>
+const ViewerTemplate = ({ viewer, spaceNavigator }) => (
+    <div className={cx('viewer-templste')}>
+        <header>
+            Astronomy Picture of the Day
+        </header>
+            <div className={cx('viewer-wrapper')}>
+                {viewer}
+                <div className={cx('space-navigator-wrapper')}>
+                    {spaceNavigator}
+                </div>
+            </div>
+    </div>
+);
 
-        </div>
-    );
-};
 
 export default ViewerTemplate;
