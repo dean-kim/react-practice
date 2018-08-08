3a2a350b58008cd625ceb54e4bf7d1d7ce0320b4
dean-kim <keunu07@gmail.com>
2018-08-08 12:49:12 +0900

    add store

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './containers/App';
+import './index.css';
 
+// Redux 관련 불러오기
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+// 스토어 생성
+const store = createStore(reducers);
 
 ReactDOM.render(<App />, document.getElementById('root'));
