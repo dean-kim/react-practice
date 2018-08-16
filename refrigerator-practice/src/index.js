41afd4da27b8f1e204cd2fa66a8d59263967edc5
dean-kim <keunu07@gmail.com>
2018-08-16 15:21:35 +0900

    fixed index

--- refrigerator-practice/src/index.js
+++ refrigerator-practice/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './containers/App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux 관련 불러오기
+import { createStore } from 'redux';
+import reducers from './modules';
+import { Provider } from 'react-redux';
+
+// 스토어 생성 및 REDUX 개발자 도구 연동
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
