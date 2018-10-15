ba706381a0b2a0c5c8ef7273169cfd50f622cc6b
dean-kim <keunu07@gmail.com>
2018-10-16 08:36:54 +0900

    add form in template

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import TodoListTemplate from './components/TodoListTemplate'
+import Form from "./components/Form";
 
 class App extends Component {
     render() {
         return (
-            <TodoListTemplate>
+            <TodoListTemplate form={<Form/>}>
                 템플릿 렌더 테스트!
             </TodoListTemplate>
         )
