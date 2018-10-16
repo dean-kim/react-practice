3908c51e1de624564bf9fa33d4559a0437b91429
dean-kim <keunu07@gmail.com>
2018-10-16 10:09:22 +0900

    add remove function

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -62,13 +62,21 @@ class App extends Component {
         })
     }
 
+    handleRemove = (id) => {
+        const { todos } = this.state
+        this.setState({
+            todos: todos.filter(todo => todo.id !== id)
+        })
+    }
+
     render() {
         const { input, todos } = this.state
         const {
             handleChange,
             handleCreate,
             handleKeyPress,
-            handleToggle
+            handleToggle,
+            handleRemove
         } = this
 
         return (
@@ -80,7 +88,7 @@ class App extends Component {
                     onClick={handleCreate}
                 />
             )}>
-                <TodoItemList todos={todos} onToggle={handleToggle} />
+                <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove} />
             </TodoListTemplate>
         )
     }
