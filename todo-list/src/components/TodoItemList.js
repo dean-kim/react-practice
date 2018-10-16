93045bc0175162b5bbb34da1e2f93857da6f8c21
dean-kim <keunu07@gmail.com>
2018-10-16 09:58:38 +0900

    map to todos array

--- todo-list/src/components/TodoItemList.js
+++ todo-list/src/components/TodoItemList.js
@@ -4,11 +4,22 @@ import TodoItem from './TodoItem'
 class TodoItemList extends Component {
     render() {
         const { todos, onToggle, onRemove } =this.props
+
+        const todoList = todos.map(
+            ({id, text, checked}) => (
+                <TodoItem
+                    id={id}
+                    text={text}
+                    checked={checked}
+                    onToggle={onToggle}
+                    onRemove={onRemove}
+                    key={id}
+                />
+            )
+        )
         return (
             <div>
-                <TodoItem text="Hello"/>
-                <TodoItem text="React"/>
-                <TodoItem text="Welcome!"/>
+                {todoList}
             </div>
         )
     }
